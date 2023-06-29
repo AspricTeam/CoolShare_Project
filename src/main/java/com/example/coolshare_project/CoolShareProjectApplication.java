fb716255d2944b10d2f3321e29ba47276178ce5e
Aspric <1394854425@qq.com>
2023-06-29 19:54:45 +0800

    新建工程

---
+++ src/main/java/com/example/coolshare_project/CoolShareProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.coolshare_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolShareProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoolShareProjectApplication.class, args);
+    }
+
+}
